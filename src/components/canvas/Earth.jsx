0e41bfed0cfd612ffc9ac54b3bc57fe32d83af61
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2023-05-31 12:00:17 -0400

    Add contact form

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 const Earth = () => {
-  return (
-    <div>Earth</div>
-  )
-}
+  return <div>Earth</div>;
+};
 
-export default Earth
+export default Earth;
